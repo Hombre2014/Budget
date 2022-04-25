7d1093cc7f45e04461ad58c43ed264936e598d86
Hombre2014 <chamkoriyski@gmail.com>
2022-04-25 17:17:15 +0200

    Add validation and allowen parameters to model and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
