5f42a1194023a12bd4e746e6e369bbce78b8bc65
Hombre2014 <chamkoriyski@gmail.com>
2022-04-26 19:49:11 +0200

    Add category method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,8 @@
 class CategoriesController < ApplicationController
-  # def index
-  #   @user = User.find(params[:user_id])
-  #   @categories = @user.categories.includes(:spendings)
-  # end
-
   def index
     @categories = current_user.categories
-    # @category = Category.find_by(id: params[:id])
+    # @user = User.find(params[:user_id])
+    # @categories = @user.categories.includes(:spendings)
   end
 
   def show
@@ -14,10 +10,51 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @category = current_user.category.includes(:spendings)
+    # @category = Category.new
+    @user = current_user
+    @category = @user.categories.new
+  end
+
+  def create
+    @user = current_user
+    @category = @user.categories.new(category_params)
+
+    if @category.save
+      flash[:notice] = 'The category have been created successfully.'
+      redirect_to "/users/#{current_user.id}/categories"
+    else
+      flash[:alert] = 'Adding a new category failed.'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # def new
-  #   @category = Category.new
+  #   @categories = current_user.categories.includes(:spendings)
   # end
+
+  # def create
+  #   @category = current_user.categories.new(category_params)
+
+  #   respond_to do |format|
+  #     if @category.save
+  #       format.html { redirect_to category_path(@category), notice: 'Category was successfully created.' }
+  #       # format.json { render :show, status: :created, location: @category }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       # format.json { render json: @category.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:user_id, :name, :icon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root "users#index"
 
-  resources :users, only: [:index] do
+  resources :users, only: [:index, :show] do
     resources :categories, only: [:index, :show, :new, :create] do
       resources :spendings, only: [:index, :show, :new, :create]
     end
   end
 
+  # post "/users//categories/new" => 'categories#create'
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy' 
   end
