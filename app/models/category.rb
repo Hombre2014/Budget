2c48c93644f410de9f95aab7b9b8c68ccc92dbc1
Hombre2014 <chamkoriyski@gmail.com>
2022-04-28 13:50:40 +0200

    Add model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :spendings
+
+  validates :name, length: { in: 1..50 }, presence: true, allow_blank: false
+  validates :icon,  presence: true, allow_blank: false
 end
--- app/models/spending.rb
+++ app/models/spending.rb
@@ -1,4 +1,7 @@
 class Spending < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, length: { in: 1..50 }, presence: true, allow_blank: false
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
 end
