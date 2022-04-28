f41ce36a5b8bf2116bcdab06f1d6091a53daee54
Hombre2014 <chamkoriyski@gmail.com>
2022-04-28 15:40:25 +0200

    Install the rspec tools and add tests for models and categories veiw

--- Gemfile
+++ Gemfile
@@ -66,6 +66,8 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'database_cleaner'
+  gem 'rspec-rails', '~> 5.0.0'
 end
 
 group :development do
@@ -77,4 +79,11 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
+end
+
+group :test do
+  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
 end
---
+++ spec/views/categories_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Testing Categories view', type: :feature do
+  describe 'Category#index' do
+    before(:each) do
+      user = User.create! name: 'Tester', email: 'tester@example.com', password: 'password'
+      visit new_user_session_path
+      fill_in 'Email', with: 'tester@example.com'
+      fill_in 'Password', with: 'password'
+      click_button 'LOG IN'
+      visit '/users/1/categories'
+    end
+
+    subject { Category.new(user_id: @user, name: 'Sport', icon: 'Sport.png') }
+    before { subject.save }
+
+    it 'can see the Add new category' do
+      expect(page).to have_content 'ADD NEW CATEGORY'
+    end
+
+    it 'should be able to see Welcome user name' do
+      expect(page).to have_content 'Welcome, Tester'
+    end
+  end
+end
